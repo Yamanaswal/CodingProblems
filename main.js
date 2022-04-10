38ad4bc19a810964c24fe6fa599ce1c4f46b9e6b
Yamanaswal <yamanaswal@gmail.com>
2022-04-10 14:33:58 +0530

    Created Coding Problems

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 
-function getSumOfNNumbers() {
-    return sum = n(n + 1) / 2
+function getSumOfNNumbers(n) {
+    return (n(n + 1)) / 2
 }
+
+// 1. Find the missing number in the array
+let find_missing = function (input) {
+    let actualSum = 0
+
+    for (let i in input) {
+        actualSum += input[i];
+    }
+
+    let sumOfN = getSumOfNNumbers(input.length)
+
+    return actualSum - sumOfN
+}
+
+
+
+let random_array = [19, 14, 2, 17, 4, 12, 20, 7, 16, 9, 13, 8, 11, 18, 3, 6, 10, 1, 15];
+let actual_missing = find_missing(random_array);
+console.log("actual missing is " + actual_missing);
