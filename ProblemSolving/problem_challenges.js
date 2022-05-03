0d7a7c4a78d8da5a76908e76f96cba4b2a761563
Yamanaswal <yamanaswal@gmail.com>
2022-05-03 13:52:56 +0530

    Added Problem Solving Challenges

---
+++ ProblemSolving/problem_challenges.js
@@ -0,0 +1,72 @@
+// Question 1: Frequency Counter Problem - sameFrequency.
+// it have same value (irrelevent to count) in Both Inputs
+// e.g.
+// sameFrequency(182, 281) // true
+//  sameFrequency(34, 14)) // false
+//  sameFrequency(3589578, 5879385)) // true
+//  sameFrequency(22, 222)) // true
+
+
+
+
+
+// Question 2: Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments,
+// and checks whether there are any duplicates among the arguments passed in.
+// e.g.
+// areThereDuplicates(1, 2, 3) //false
+// areThereDuplicates(1, 2, 2) //true
+// areThereDuplicates("c", "a", "t", "s", "s") //true
+
+
+
+
+
+
+// Question 3: Multiple Pointers - averagePair
+// Given a sorted array of integers and a target average,
+// determine if there is a pair of values in the array,
+// where the average of the pair equals the target average.
+// There may be more than one pair that matches the average target, but we just want the first pair.
+
+// Example: If the given array is [1, 2, 3 , 4 ,5 , 6] and the target average is 2, then we want the algorithm to return true. This is because the average of 1 and 2 is 2.
+// But if none of the numbers in the array can create the target average of 2, then return false.
+
+
+
+
+
+
+// Question 4: Multiple Pointers - isSubsequence
+// Given two strings s and t,
+// check if s is a subsequence of t.
+// A subsequence can be formed by deleting
+// some characters in a string without changing the order of the other characters.
+
+// Input: s = "abc", t = “ahbgdc“
+// Output: true
+
+
+
+
+
+// Question 5: Sliding Window Maximum (Maximum of all subarrays of size k)
+//  Given an array of integers and a number k, find the maximum sum of a subarray of size k.
+
+// Eg: For given array A[] = {10,30,20,50,60,40,40} of size k = 2
+// The maximum sum subarray would be
+// sum = 50 + 60 = 100.
+
+
+
+
+
+
+// Question 6: Given an array of positive integers,
+// find the smallest subarray’s length whose sum of elements is greater than a given number k.
+
+// Input:  {1, 2, 3, 4, 5, 6, 7, 8}, k = 20
+// Output: The smallest subarray length is 3
+// Explanation: The smallest subarray with sum > 20 is {6, 7, 8}
+
+
