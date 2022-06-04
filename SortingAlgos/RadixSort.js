b792e5cf98f06cf5b0a1168370fd12de85448388
Yamanaswal <yamanaswal@gmail.com>
2022-06-04 22:26:06 +0530

    Quick Sort Added

--- SortingAlgos/RadixSort.js
+++ SortingAlgos/RadixSort.js
@@ -24,6 +24,25 @@ function mostDigits(nums) {
 console.log(mostDigits([23, 567, 89, 12234324, 90]));
 console.log(getDigit(12234324, 3));
 
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums);
+
+
+    for (let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({ length: 10 }, () => []);
+
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k);
+            digitBuckets[digit].push(nums[i]);
+        }
+
+        nums = [].concat(...digitBuckets);
+    }
+
+    return nums;
+}
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
 
 
 
